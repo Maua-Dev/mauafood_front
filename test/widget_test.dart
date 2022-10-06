5635a8a666be90f199e29bad1b8739281acb01af
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-10-06 00:13:25 -0300

    Add test file to fix error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mauafood_front/app/app_widget.dart';
 
 main() {
-  test('Test', () {});
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const AppWidget());
+  });
 }

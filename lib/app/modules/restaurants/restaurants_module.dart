a1d285449067b1fac01a97816f03b03c20577cd3
Caio Correia <caiomc03@gmail.com>
2022-10-20 22:33:36 -0300

    UI adjusts

--- lib/app/modules/restaurants/restaurants_module.dart
+++ lib/app/modules/restaurants/restaurants_module.dart
@@ -2,7 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mauafood_front/app/modules/menu/menu_module.dart';
 import 'package:mauafood_front/app/modules/restaurants/domain/usecases/get_restaurant.dart';
 import 'package:mauafood_front/app/modules/restaurants/presenter/controllers/restaurant_controller.dart';
-import 'package:mauafood_front/app/modules/restaurants/ui/pages/restaurants_page.dart';
+import 'package:mauafood_front/app/modules/restaurants/presenter/ui/pages/restaurants_page.dart';
 
 class RestaurantModule extends Module {
   @override

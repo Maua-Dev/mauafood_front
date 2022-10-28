7b84cf7c8e5f61ff2e983e4f449c6d48a5e68a89
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-10-28 01:39:04 -0300

    Setting up Amplify

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mauafood_front/amplifyconfiguration.dart';
 import 'package:mauafood_front/app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await configureAmplify();
   return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+}
+
+Future configureAmplify() async {
+  Amplify.addPlugins([AmplifyAuthCognito()]);
+
+  try {
+    await Amplify.configure(amplifyconfig);
+  } catch (e) {
+    print('Amplify is already configured');
+  }
 }

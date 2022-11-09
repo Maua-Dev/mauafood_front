d7779a1e329e64e441e9d64b265c0fb88e721339
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-11-08 21:29:12 -0300

    modular

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mauafood_front/app/modules/auth/domain/infra/auth_storage_interface.dart';
+import 'package:mauafood_front/app/modules/auth/domain/usecases/confirm_reset_password.dart';
 import 'package:mauafood_front/app/modules/auth/infra/repository/auth_storage_impl.dart';
 import 'package:mauafood_front/app/modules/auth/presenter/bloc/auth/auth_bloc.dart';
 import 'package:mauafood_front/app/modules/auth/presenter/bloc/confirm-email/confirm_email_bloc.dart';
@@ -14,6 +15,7 @@ import 'domain/usecases/confirm_email.dart';
 import 'domain/usecases/login_user.dart';
 import 'domain/usecases/logout_user.dart';
 import 'domain/usecases/register_user.dart';
+import 'domain/usecases/reset_password.dart';
 import 'infra/datasources/auth_datasouce_interface.dart';
 import 'infra/repository/auth_repository_impl.dart';
 
@@ -28,6 +30,11 @@ class AuthModule extends Module {
             export: true),
         Bind<LogoutUserInterface>((i) => LogoutUserImpl(repository: i()),
             export: true),
+        Bind<ResetPasswordInterface>((i) => ResetPasswordImpl(repository: i()),
+            export: true),
+        Bind<ConfirmResetPasswordInterface>(
+            (i) => ConfirmResetPasswordImpl(repository: i()),
+            export: true),
         AsyncBind<AuthStorageInterface>((i) => AuthStorageImpl.instance()),
         Bind<AuthBloc>(
             (i) => AuthBloc(
@@ -36,6 +43,8 @@ class AuthModule extends Module {
                   register: i(),
                   confirmEmail: i(),
                   storage: i(),
+                  confirmResetPassword: i(),
+                  resetPassword: i(),
                 ),
             export: true),
         AsyncBind<AuthStorageInterface>((i) => AuthStorageImpl.instance(),

630eeca49a675e93d9dd6f3b5a11e83715a13a7d
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-11-11 00:07:13 -0300

    domain use cases tests

---
+++ test/app/modules/auth/domain/usecases/forgot_password_test.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mauafood_front/app/modules/auth/domain/errors/auth_errors.dart';
+import 'package:mauafood_front/app/modules/auth/domain/infra/auth_repository_interface.dart';
+import 'package:mauafood_front/app/modules/auth/domain/usecases/forgot_password.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'forgot_password_test.mocks.dart';
+
+@GenerateMocks([AuthRepositoryInterface])
+void main() {
+  late ForgotPasswordInterface useCase;
+  AuthRepositoryInterface repository = MockAuthRepositoryInterface();
+  String email = '';
+
+  setUp(() {
+    useCase = ForgotPasswordImpl(repository: repository);
+  });
+
+  test('[TEST] - forgot password returns success bool', () async {
+    when(repository.forgotPassword(email)).thenAnswer(
+      (realInvocation) async => const Right(true),
+    );
+    var result = await useCase(email);
+    expect(result.fold(id, id), isA<void>());
+  });
+
+  test('[TEST] - forgot password returns error', () async {
+    when(repository.forgotPassword(email)).thenAnswer(
+      (realInvocation) async => Left(ForgotPasswordError(message: '')),
+    );
+    var result = await useCase(email);
+    expect(result.fold(id, id), isA<ForgotPasswordError>());
+  });
+}

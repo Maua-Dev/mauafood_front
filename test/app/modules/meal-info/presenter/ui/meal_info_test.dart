58ddbc65bd34b67bc0cb249837a1d932067d3a9b
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2023-01-16 11:50:09 -0300

    created test architeture

---
+++ test/app/modules/meal-info/presenter/ui/meal_info_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter/material.dart';
+// import 'package:mauafood_front/app/modules/meal-info/meal_info_module.dart';
+// import 'package:mauafood_front/app/modules/meal-info/presenter/ui/meal_info_page.dart';
+// import 'package:mauafood_front/app/modules/menu/presenter/bloc/menu_bloc.dart';
+
+void main() {
+  // final MealInfoModule module = MealInfoModule();
+  // final bloc = MenuBloc();
+
+  testWidgets('MealInfoPage has title', (widgetTester) async {
+    //  await tester.pumpWidget(buildTestableWidget(MealInfoPage(title: 'MealInfo')));
+    //  final titleFinder = find.text('MealInfo');
+    //  expect(titleFinder, findsOneWidget);
+
+    // await widgetTester.pumpWidget(MaterialApp(
+    //     home: BlocProvider(
+    //       create: (context) => bloc,
+    //       child: const MealInfoPage(bloc: bloc),
+    //     ),
+    //   ));
+  });
+}

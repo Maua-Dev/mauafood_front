65832b843df109915dec0663d7b24a7b9a478898
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-05-17 03:23:02 -0300

    contact with mobx (need fix usecase)

--- lib/app/modules/menu/presenter/ui/user/widgets/contact/text_field_contact_widget.dart
+++ lib/app/modules/menu/presenter/ui/user/widgets/contact/text_field_contact_widget.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_form_bloc/flutter_form_bloc.dart';
 
 import '../../../../../../../shared/themes/app_colors.dart';
 import '../../../../../../../shared/themes/app_text_styles.dart';
 
 class TextFieldContactWidget extends StatelessWidget {
-  final TextFieldBloc textFieldBloc;
   final String title;
   final TextInputType? keyboardType;
   final Iterable<String>? autoFillHints;
-  final SuffixButton? suffixButton;
   final String? hintText;
   final List<TextInputFormatter>? inputFormatters;
+  final String? Function(String?)? validator;
 
   const TextFieldContactWidget(
       {super.key,
-      required this.textFieldBloc,
       required this.title,
       this.keyboardType,
       this.autoFillHints,
-      this.suffixButton,
       this.hintText,
       this.inputFormatters,
-      required String? Function(String? value) validator});
+      this.validator});
 
   @override
   Widget build(BuildContext context) {
@@ -37,15 +33,15 @@ class TextFieldContactWidget extends StatelessWidget {
             title,
             style: AppTextStyles.h2,
           ),
-          TextFieldBlocBuilder(
+          TextFormField(
             autofocus: false,
             maxLines: null,
             textAlign: TextAlign.start,
-            textFieldBloc: textFieldBloc,
             expands: false,
             keyboardType: TextInputType.text,
             cursorColor: AppColors.mainBlueColor,
-            textStyle: AppTextStyles.h2,
+            style: AppTextStyles.h2,
+            validator: validator,
             decoration: InputDecoration(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 12),
                 hintText: hintText,

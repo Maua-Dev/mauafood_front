bf620f6641a398803023076277924fba85ef103f
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-05-17 12:46:19 -0300

    change password page

---
+++ lib/app/modules/auth/presenter/controllers/change_password/change_password_controller.dart
@@ -0,0 +1,84 @@
+import 'package:mobx/mobx.dart';
+
+import '../../../../../../generated/l10n.dart';
+import '../../../domain/usecases/confirm_reset_password.dart';
+import '../../ui/states/change_password_state.dart';
+
+part 'change_password_controller.g.dart';
+
+class ChangePasswordController = ChangePasswordControllerBase
+    with _$ChangePasswordController;
+
+abstract class ChangePasswordControllerBase with Store {
+  final ConfirmResetPasswordInterface _confirmResetPassword;
+
+  ChangePasswordControllerBase(this._confirmResetPassword);
+
+  @observable
+  ChangePasswordState state = ChangePasswordInitialState();
+
+  @action
+  void changeState(ChangePasswordState value) => state = value;
+
+  @observable
+  String code = '';
+
+  @action
+  void setCode(String value) => code = value;
+
+  @observable
+  String newPassword = '';
+
+  @action
+  void setNewPassword(String value) => newPassword = value;
+
+  @observable
+  String confirmNewPassword = '';
+
+  @action
+  void setConfirmNewPassword(String value) => confirmNewPassword = value;
+
+  @action
+  String? validatePassword(String? value) {
+    String pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    RegExp regExp = RegExp(pattern);
+    if (value!.isEmpty) {
+      return S.current.requiredFieldAlert;
+    } else if (!regExp.hasMatch(value)) {
+      return S.current.passwordInstructionsAlert;
+    }
+    return null;
+  }
+
+  @action
+  String? validateConfirmPassword(String? value) {
+    if (value!.isEmpty) {
+      return S.current.requiredFieldAlert;
+    } else if (value != newPassword) {
+      return S.current.equalPasswordAlert;
+    }
+    return null;
+  }
+
+  @action
+  Future<void> changePassword() async {
+    state = ChangePasswordLoadingState();
+    var result = await _confirmResetPassword('', code, newPassword);
+    state = result.fold((failure) {
+      return ChangePasswordErrorState(error: failure);
+    }, (isConfirmed) {
+      return const ChangePasswordSuccessState();
+    });
+  }
+
+  @action
+  String? validateCode(String? value) {
+    if (value!.isEmpty) {
+      return S.current.requiredFieldAlert;
+    } else if (value.length < 6) {
+      return S.current.codeErrorAlert;
+    }
+    return null;
+  }
+}
---
+++ lib/app/modules/auth/presenter/ui/states/change_password_state.dart
@@ -0,0 +1,19 @@
+import '../../../domain/errors/auth_errors.dart';
+
+abstract class ChangePasswordState {
+  const ChangePasswordState();
+}
+
+class ChangePasswordInitialState extends ChangePasswordState {}
+
+class ChangePasswordLoadingState extends ChangePasswordState {}
+
+class ChangePasswordSuccessState extends ChangePasswordState {
+  const ChangePasswordSuccessState();
+}
+
+class ChangePasswordErrorState extends ChangePasswordState {
+  final AuthErrors error;
+
+  const ChangePasswordErrorState({required this.error});
+}

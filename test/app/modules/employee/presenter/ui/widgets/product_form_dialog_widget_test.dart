7a2db26d826a51542d50d8eb1466740a0585f394
EnricoSantarelli <63019527+EnricoSantarelli@users.noreply.github.com>
2023-07-20 11:35:33 -0300

    linter

--- test/app/modules/employee/presenter/ui/widgets/product_form_dialog_widget_test.dart
+++ test/app/modules/employee/presenter/ui/widgets/product_form_dialog_widget_test.dart
@@ -7,20 +7,17 @@ import 'package:mauafood_front/app/modules/employee/presenter/controllers/menu/e
 import 'package:mauafood_front/app/modules/employee/presenter/controllers/product-form/product_form_controller.dart';
 import 'package:mauafood_front/app/modules/employee/presenter/states/product-form/product_form_state.dart';
 import 'package:mauafood_front/app/modules/employee/presenter/ui/widgets/product_form_dialog_widget.dart';
-import 'package:mauafood_front/app/shared/domain/enums/product_enum.dart';
 import 'package:mauafood_front/app/shared/domain/enums/restaurant_enum.dart';
 import 'package:mauafood_front/app/shared/domain/usecases/create_product_usecase.dart';
 import 'package:mauafood_front/app/shared/domain/usecases/delete_product_usecase.dart';
 import 'package:mauafood_front/app/shared/domain/usecases/get_restaurant_product_usecase.dart';
 import 'package:mauafood_front/app/shared/domain/usecases/update_product_usecase.dart';
-import 'package:mauafood_front/app/shared/infra/models/product_model.dart';
 import 'package:mauafood_front/generated/l10n.dart';
 import 'package:mockito/annotations.dart';
 import 'package:modular_test/modular_test.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 
 import '../../controllers/menu/employee_menu_restaurant_controller_test.dart';
-import '../pages/employee_menu_page_test.mocks.dart';
 import 'product_form_dialog_widget_test.mocks.dart';
 
 @GenerateMocks([IUpdateProductUsecase, ICreateProductUsecase])
@@ -34,30 +31,6 @@ void main() {
   IDeleteProductUsecase deleteProductSuccessUsecase =
       DeleteProductMockSuccess();
   RestaurantEnum restaurantInfo = RestaurantEnum.souza_de_abreu;
-
-  ProductModel testMock = ProductModel(
-    id: '0',
-    name: 'name',
-    description: 'description',
-    price: 10,
-    type: ProductEnum.CANDIES,
-    photo: '',
-    available: true,
-    lastUpdate: DateTime.now(),
-  );
-  var listMock = [
-    testMock,
-    ProductModel(
-      id: '0',
-      name: 'name',
-      description: 'description',
-      price: 10,
-      type: ProductEnum.CANDIES,
-      photo: '',
-      available: true,
-      lastUpdate: DateTime.now(),
-    ),
-  ];
 
   setUpAll(() async {
     await S.load(const Locale.fromSubtags(languageCode: 'en'));

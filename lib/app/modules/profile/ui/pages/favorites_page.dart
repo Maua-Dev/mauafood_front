8203884ba294c81dedc80ed25f7dff014b5156cf
Breno Amorim <102449371+AmorimBreno@users.noreply.github.com>
2023-07-20 16:09:01 -0300

    favorites page figma design

--- lib/app/modules/profile/ui/pages/favorites_page.dart
+++ lib/app/modules/profile/ui/pages/favorites_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mauafood_front/app/shared/helpers/utils/screen_helper.dart';
 import 'package:mauafood_front/app/shared/themes/app_colors.dart';
 
 class FavoritesPage extends StatelessWidget {
@@ -9,29 +10,90 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-        shadowColor: Colors.transparent,
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        title: const Text("Favoritos", style: TextStyle(fontSize: 20, color: Colors.black),),
-        leading: SizedBox(
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(ScreenHelper.height(context) * 0.15),
           child: Padding(
-            padding: const EdgeInsets.only(top: 12.0, left: 8),
-            child: Container(width: 50, color: AppColors.mainBlueColor, child: IconButton(
-              alignment: Alignment.topLeft,
-              onPressed:  () => Modular.to.navigate('/landing/profile/'), icon: Icon(FontAwesomeIcons.arrowLeft, color: AppColors.white,))),
+            padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 8),
+            child: Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: AppColors.mainBlueColor,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(8))),
+                    child: IconButton(
+                        onPressed: () =>
+                            Modular.to.navigate('/landing/profile/'),
+                        icon: Icon(
+                          FontAwesomeIcons.arrowLeft,
+                          color: AppColors.white,
+                          size: 23,
+                        ))),
+                SizedBox(
+                  width: ScreenHelper.width(context) * 0.24,
+                ),
+                const SizedBox(
+                  child: Text(
+                    "Favoritos",
+                    style: TextStyle(
+                        fontSize: 28,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
           )),
-      ),
       backgroundColor: AppColors.backgroundColor2,
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Container(height: 50, width: 75, color: AppColors.mainBlueColor), const Column(children: [Text("Macarronada"), Text("Restaurante do H")]), Icon(FontAwesomeIcons.solidHeart, color: AppColors.mainBlueColor,)],)
-          ],
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32),
+        child: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return SizedBox(
+                height: 120,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                            color: Colors.black),
+                      ),
+                      SizedBox(
+                        width: ScreenHelper.width(context) * 0.45,
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Macarronada",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            Text(
+                              "Restaurante do H",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Icon(
+                        FontAwesomeIcons.solidHeart,
+                        size: 32,
+                        color: AppColors.mainBlueColor,
+                      ),
+                    ],
+                  ),
+                ));
+          },
+          itemCount: 2,
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider();
+          },
         ),
       ),
     );

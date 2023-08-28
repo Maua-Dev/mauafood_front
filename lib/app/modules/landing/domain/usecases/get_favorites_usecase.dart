c04d0cb3c6571abf342dbcb396bf73fe21c61e98
Breno Amorim <102449371+AmorimBreno@users.noreply.github.com>
2023-08-28 19:22:41 -0300

    getfavorites usecase

---
+++ lib/app/modules/landing/domain/usecases/get_favorites_usecase.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:mauafood_front/app/modules/landing/domain/repositories/favorites_repositorie_interface.dart';
+
+import '../../../../shared/domain/entities/product.dart';
+import '../../../../shared/helpers/errors/errors.dart';
+
+abstract class GetFavoritesUsecase {
+  Future<Either<Failure, List<Product>>> call();
+}
+
+class GetFavoritesUsecaseImpl implements GetFavoritesUsecase {
+  final FavoritesRepository repository;
+
+  GetFavoritesUsecaseImpl({required this.repository});
+
+  @override
+  Future<Either<Failure, List<Product>>> call() async {
+    var result = await repository.getFavorites();
+    return result.fold((failureResult) => result, (listResult) async {
+      return result.where((r) => r.isNotEmpty, () => EmptyList());
+    });
+  }
+}

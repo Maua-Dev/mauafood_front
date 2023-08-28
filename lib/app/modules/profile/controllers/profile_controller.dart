744f2946d64a6c1d4460143012de68a22ccc2acc
Breno Amorim <102449371+AmorimBreno@users.noreply.github.com>
2023-08-28 20:08:33 -0300

    getFavorites on the profile controller

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,4 +1,5 @@
 import 'package:auth_package/core/auth_store.dart';
+import 'package:mauafood_front/app/modules/profile/domain/usecases/get_favorites_usecase.dart';
 import 'package:mobx/mobx.dart';
 part 'profile_controller.g.dart';
 
@@ -6,9 +7,20 @@ class ProfileController = ProfileControllerBase with _$ProfileController;
 
 abstract class ProfileControllerBase with Store {
   final AuthStore _authStore;
+  late GetFavoritesUsecaseImpl _getFavorites;
+
+  var favoritesList = [];
 
   ProfileControllerBase(this._authStore);
 
   @computed
   String get name => _authStore.user?.email ?? '';
+
+  Future<void> getFavorites() async {
+    var result = await _getFavorites();
+
+    result.fold((failure) => null, (successList) async {
+      favoritesList = successList;
+    });
+  }
 }

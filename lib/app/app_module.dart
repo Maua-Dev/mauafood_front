c02f3366b86998391e8b3869e5bf29c50304b305
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2024-03-13 19:56:39 -0300

    fix

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -16,7 +16,8 @@ import 'shared/helpers/services/http/http_request_interface.dart';
 
 class AppModule extends Module {
   @override
-  List<Module> get imports => [MicroAppAuthModule(amplifyconfig)];
+  List<Module> get imports =>
+      [MicroAppAuthModule(amplifyconfig: amplifyconfig)];
 
   @override
   List<Bind<Object>> get binds => [

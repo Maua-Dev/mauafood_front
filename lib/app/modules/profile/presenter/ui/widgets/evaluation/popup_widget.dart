f92fd381683f4cd70178d65042dda4447f1f7467
Breno Amorim <102449371+AmorimBreno@users.noreply.github.com>
2024-04-19 12:36:01 -0300

    popUp equal to figma

--- lib/app/modules/profile/presenter/ui/widgets/evaluation/popup_widget.dart
+++ lib/app/modules/profile/presenter/ui/widgets/evaluation/popup_widget.dart
@@ -19,66 +19,76 @@ class PopUpWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Observer(builder: (_) {
       return AlertDialog(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-        content: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  icon: const Icon(Icons.close_rounded),
-                  iconSize: 40.0,
-                  color: AppColors.mainBlueColor,
-                  onPressed: () {
-                    controller.togglePopup(); // Redirects user to profile page
-                  },
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  restaurantcontroller.restaurantCart
-                      .restaurantName, // contoller cart - Restaurant Name
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.letterColor,
+        contentPadding: const EdgeInsets.all(0),
+        actionsPadding: const EdgeInsets.all(0),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+        content: SizedBox(
+          width: 350,
+          height: 320,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    icon: const Icon(Icons.close_rounded),
+                    iconSize: 40.0,
+                    color: AppColors.mainBlueColor,
+                    onPressed: () {
+                      controller
+                          .togglePopup(); // Redirects user to profile page
+                    },
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    restaurantcontroller.restaurantCart.restaurantName.isEmpty
+                        ? "Nome do Restaurante"
+                        : restaurantcontroller.restaurantCart
+                            .restaurantName, // contoller cart - Restaurant Name
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w900,
+                      color: AppColors.letterColor,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                    child: Text(
-                  "Avalie o restaurante, seu feedback é importante! ",
-                  style: TextStyle(
-                    fontSize: ScreenHelper.width(context) < 400 ? 16.0 : 24.0,
-                    color: AppColors.letterThinColor,
-                  ),
-                )),
-              ],
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
                 child: SizedBox(
-                  height: 50,
-                  child: Image.network(
-                    greyLogo,
-                    fit: BoxFit.contain,
+                  width: 250,
+                  child: Text(
+                    "Avalie o restaurante, seu feedback é importante! ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: ScreenHelper.width(context) < 400 ? 16.0 : 20.0,
+                      color: AppColors.letterThinColor,
+                    ),
                   ),
                 ),
               ),
-            ),
-            StarsWidget(
-                controller: controller) // display StarsWidget - rating stars
-          ],
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    height: 50,
+                    child: Image.network(
+                      greyLogo,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+              StarsWidget(
+                  controller: controller) // display StarsWidget - rating stars
+            ],
+          ),
         ),
         actions: [
           Center(
--- lib/app/modules/profile/presenter/ui/widgets/evaluation/stars_widget.dart
+++ lib/app/modules/profile/presenter/ui/widgets/evaluation/stars_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mauafood_front/app/modules/profile/presenter/controllers/popup_controller.dart';
 import 'package:mauafood_front/app/shared/themes/app_colors.dart';
 
@@ -10,52 +9,71 @@ class StarsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (_) {
-      return Row(
+    double iconSize = 56;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           GestureDetector(
             onTap: () => controller.setStars(1),
-            child: Icon(Icons.star_border_rounded,
-                size: 48.0,
-                color: controller.grade >= 1
-                    ? AppColors.yellowColor
-                    : AppColors.letterColor),
+            child: Icon(
+                controller.grade >= 1
+                    ? Icons.star_rounded
+                    : Icons.star_border_rounded,
+                size: iconSize,
+                weight: 0.01,
+                color: controller.grade != 0
+                    ? AppColors.mainBlueColor
+                    : Colors.grey),
           ),
           GestureDetector(
             onTap: () => controller.setStars(2),
-            child: Icon(Icons.star_border_rounded,
-                size: 48.0,
-                color: controller.grade >= 2
-                    ? AppColors.yellowColor
-                    : AppColors.letterColor),
+            child: Icon(
+                controller.grade >= 2
+                    ? Icons.star_rounded
+                    : Icons.star_border_rounded,
+                size: iconSize,
+                color: controller.grade != 0
+                    ? AppColors.mainBlueColor
+                    : Colors.grey),
           ),
           GestureDetector(
             onTap: () => controller.setStars(3),
-            child: Icon(Icons.star_border_rounded,
-                size: 48.0,
-                color: controller.grade >= 3
-                    ? AppColors.yellowColor
-                    : AppColors.letterColor),
+            child: Icon(
+                controller.grade >= 3
+                    ? Icons.star_rounded
+                    : Icons.star_border_rounded,
+                size: iconSize,
+                color: controller.grade != 0
+                    ? AppColors.mainBlueColor
+                    : Colors.grey),
           ),
           GestureDetector(
             onTap: () => controller.setStars(4),
-            child: Icon(Icons.star_border_rounded,
-                size: 48.0,
-                color: controller.grade >= 4
-                    ? AppColors.yellowColor
-                    : AppColors.letterColor),
+            child: Icon(
+                controller.grade >= 4
+                    ? Icons.star_rounded
+                    : Icons.star_border_rounded,
+                size: iconSize,
+                color: controller.grade != 0
+                    ? AppColors.mainBlueColor
+                    : Colors.grey),
           ),
           GestureDetector(
             onTap: () => controller.setStars(5),
-            child: Icon(Icons.star_border_rounded,
-                size: 48.0,
-                color: controller.grade == 5
-                    ? AppColors.yellowColor
-                    : AppColors.letterColor),
+            child: Icon(
+                controller.grade == 5
+                    ? Icons.star_rounded
+                    : Icons.star_border_rounded,
+                size: iconSize,
+                color: controller.grade != 0
+                    ? AppColors.mainBlueColor
+                    : Colors.grey),
           ),
         ],
-      );
-    });
+      ),
+    );
   }
 }
